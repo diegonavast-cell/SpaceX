c668db4e3f8f9228879f6bc2d05cf5752239eaf4
diegonavast-cell <diego.navast@gmail.com>
2026-09-02 08:17:22 -0600

    Añadir funcion parami carrrera para la opcion de volver

---
+++ MiCarrea.js
@@ -0,0 +1,6 @@
+function confirmarCierreSesion() {
+    let respuesta = confirm("¿Estás seguro de cerrar sesión?");
+    if (respuesta) {
+      window.location.replace("file:///C:/Users/Computadrora%2010/Desktop/SpaceX/Index.html"); 
+    }
+  }

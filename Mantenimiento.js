97909e505b18e1a6ac7f32a99885d97e2ab363ce
diegonavast-cell <diego.navast@gmail.com>
2026-09-02 09:39:10 -0600

    Correccion de codigo js

--- Mantenimiento.js
+++ Mantenimiento.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 function confirmarCierreSesion() {
     let respuesta = confirm("¿Estás seguro de cerrar sesión?");
     if (respuesta) {
       window.location.replace("index.html"); 
     }
   }
-=======
-function volver(){
-  alert ("Volviendo a Mi carrera..")
-  window.location.replace("file:///C:/Users/Computadrora%2010/Desktop/SpaceX/MiCarrera.html")
-}
->>>>>>> da698374c1b231dbed3d860b1cc1a9c21f730b56

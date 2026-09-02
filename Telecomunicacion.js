a0a4926140004c696d608a64f18c3081a8558de2
diegonavast-cell <diego.navast@gmail.com>
2026-09-02 08:22:11 -0600

    Pequeño ajuste

--- Telecomunicacion.js
+++ Telecomunicacion.js
@@ -1,5 +1,5 @@
 
 function volver(){
   alert ("Volviendo a Mi carrera..")
-  window.location.replace()
+  window.location.replace("file:///C:/Users/Computadrora%2010/Desktop/SpaceX/MiCarrera.html")
 }
